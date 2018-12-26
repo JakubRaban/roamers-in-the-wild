de3b03becf6bbd3931ef71ef7fe9425812c84145
Jakub Raban <jakub.raban@gmail.com>
2018-12-26 17:41:08 +0100

    Added constructor to MapDirection

--- src/main/java/pl/jakubraban/evolutionsimulator/map/MapDirection.java
+++ src/main/java/pl/jakubraban/evolutionsimulator/map/MapDirection.java
@@ -6,7 +6,30 @@ import java.util.List;
 
 public enum MapDirection {
 
-    NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST;
+    NORTH(0,1), NORTH_EAST(1,1), EAST(1,0), SOUTH_EAST(1,-1),
+    SOUTH(0,-1), SOUTH_WEST(-1,-1), WEST(-1,0), NORTH_WEST(-1,1);
+
+    private int xStep, yStep;
+
+    MapDirection(int xStep, int yStep) {
+        this.xStep = xStep;
+        this.yStep = yStep;
+    }
+
+    public int getXStep() {
+        return xStep;
+    }
+
+    public int getYStep() {
+        return yStep;
+    }
+
+    public MapDirection directionAfterTurning(MoveDirection moveDirection) {
+        int thisDirectionNumber = this.ordinal();
+        int moveDirectionNumber = moveDirection.ordinal();
+        int newMapDirectionOrdinal = (thisDirectionNumber + moveDirectionNumber) % 8;
+        return MapDirection.values()[newMapDirectionOrdinal];
+    }
 
     public String toString() {
         return this.name();

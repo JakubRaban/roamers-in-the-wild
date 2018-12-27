3c0768124baf533b288610df5eb67a5b18854027
Jakub Raban <jakub.raban@gmail.com>
2018-12-27 22:30:12 +0100

    Added MapVisualizer class

---
+++ src/main/java/pl/jakubraban/evolutionsimulator/MapVisualizer.java
@@ -0,0 +1,34 @@
+package pl.jakubraban.evolutionsimulator;
+
+import pl.jakubraban.evolutionsimulator.entities.Animal;
+import pl.jakubraban.evolutionsimulator.map.Position;
+import pl.jakubraban.evolutionsimulator.map.WorldMap;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class MapVisualizer {
+
+    WorldMap worldMap;
+
+    public MapVisualizer(WorldMap worldMap) {
+        this.worldMap = worldMap;
+    }
+
+    public String visualize() {
+        Set<Position> positionsOfAnimals = worldMap.getAnimals().stream().map(Animal::getPosition).collect(Collectors.toSet());
+        Set<Position> positionsOfPlants = worldMap.getPlants().keySet();
+        StringBuilder mapVisualization = new StringBuilder();
+        for(int x = 0; x < worldMap.getWidth(); x++) {
+            for(int y = worldMap.getHeight() - 1; y >= 0; y--) {
+                Position currentPosition = new Position(x, y);
+                if(positionsOfAnimals.contains(currentPosition)) mapVisualization.append("M");
+                else if(positionsOfPlants.contains(currentPosition)) mapVisualization.append("*");
+                else mapVisualization.append(".");
+            }
+            mapVisualization.append("\n");
+        }
+        return mapVisualization.toString();
+    }
+
+}

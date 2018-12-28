bde0d215c3a69cf570ba802ecfc9038b22a8e8f0
Jakub Raban <jakub.raban@gmail.com>
2018-12-28 20:10:23 +0100

    Added main method

--- src/main/java/pl/jakubraban/evolutionsimulator/World.java
+++ src/main/java/pl/jakubraban/evolutionsimulator/World.java
@@ -8,7 +8,7 @@ public class World {
     private int daysElapsed = 0;
 
     public World() {
-        worldMap = new WorldMap(100, 30);
+        worldMap = new WorldMap(100,30);
     }
 
     public void simulateNewDay() {
@@ -17,8 +17,38 @@ public class World {
         worldMap.moveAnimals();
         worldMap.bringAnimalsBackToMap();
         worldMap.feedAnimals();
-        worldMap.reproduceAnimals(daysElapsed);
+        worldMap.reproduceAnimals();
         daysElapsed++;
+    }
+
+    public int getDaysElapsed() {
+        return daysElapsed;
+    }
+
+    public void bringLife() {
+        worldMap.addAnimal();
+    }
+
+    public WorldMap getWorldMap() {
+        return worldMap;
+    }
+
+    public static void main(String ... args) {
+        World world = new World();
+        Window window = new Window();
+        MapVisualizer visualizer = new MapVisualizer(world.getWorldMap());
+        world.bringLife();
+        // for(int i = 0; i < 1000; i++) world.simulateNewDay();
+        while(true) {
+            world.simulateNewDay();
+            window.setText(visualizer.visualize() + "\n\nDzień " + world.daysElapsed);
+            try {
+                if(world.daysElapsed % 5000 == 0) Thread.sleep(5000);
+                else Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
